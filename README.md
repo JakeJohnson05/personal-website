d305ee3f52d4b5e513a2914967d42f4909717fcb
Jake Johnson <jakejohnson801@outlook.com>
2020-02-08 18:41:53 -0700

    Complete package updates. Added status logger. Configured database and server setup in server.ts and database.ts. Fixed the tsconfig.server.json compiler for building the ts files. Configured nodemon with the prestart commands.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Hello,
 This is my personal website.
 
-This application is built on Angular (version 8) and Express (version 4).
+This application is built on Angular (version 9) and Express (version 4).
 
 The Angular application is served using Express. The Express server `server.ts` is run using node after tsc js compilation.
--- server.ts
+++ server.ts
@@ -8,29 +8,26 @@
 // Configure environment vars
 import { config as dotenvConfig } from 'dotenv';
 dotenvConfig();
-
 import * as express from 'express';
 import { join } from 'path';
 import * as helmet from 'helmet';
 import { createServer } from 'http';
 import { json, urlencoded } from 'body-parser';
+import { success } from '@codexist/status-reports';
 
 /** The Sequelize ORM instance to interact with the database */
-import { database } from './database';
-
+import { sequelizeDB } from './database';
 /** Establishes session with the client with cookies */
 import * as session from 'express-session';
 /** Establishes configuration for cookies on the database side */
 import * as sequelizeStore from 'connect-session-sequelize';
 /** The Combined Power of the above two imports to control user sessions */
 const SequelizeSessionStore = sequelizeStore(session.Store);
-
 /** Our api to handle contact requests and rate limiting */
 import { ApiServer } from './api/api-control';
 
 /** The exress aplication instance */
 const app: express.Application = express();
-
 
 ////////////////////////////////////////////////
 // Middleware
@@ -44,23 +41,22 @@ app.use(urlencoded({ extended: false }));
 const msInHour = 3600000;
 // set up the session and config for cookies
 app.use(session({
-  secret: process.env.SECRET_KEY!,
-  resave: false,
-  saveUninitialized: false,
-  store: new SequelizeSessionStore({
-    db: database.sequelize,
-    checkExpirationInterval: msInHour * 96
-  }),
-  cookie: {
-    sameSite: true,
-    secure: false,
-    maxAge: msInHour * 24,
-    httpOnly: true
-  }
+	secret: process.env.SECRET_KEY!,
+	resave: false,
+	saveUninitialized: false,
+	store: new SequelizeSessionStore({
+		db: sequelizeDB.db,
+		checkExpirationInterval: msInHour * 96
+	}),
+	cookie: {
+		sameSite: true,
+		secure: false,
+		maxAge: msInHour * 24,
+		httpOnly: true
+	}
 }));
 // protect against dangerous web vulnerabilities
 app.use(helmet());
-
 
 ////////////////////////////////////////////////
 // Routes
@@ -72,7 +68,6 @@ app.use('/api', ApiServer());
 // Route all other routes to the angular application
 app.get('*', (_, res) => res.sendFile(join(__dirname, 'src/index.html')));
 
-
 ////////////////////////////////////////////////////////////////////////
 // Start up the Node Server
 
@@ -81,4 +76,4 @@ const port = process.env.PORT || '3000';
 app.set('port', port);
 
 // Create and then listen to output
-createServer(app).listen(port, () => console.log(`Server running on localhost:${port}`));
+createServer(app).listen(port, success('Server running', `listening on localhost:${port}`));
